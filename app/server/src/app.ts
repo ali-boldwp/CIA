b843d4bae571ba34e2841ecf6fb057f23a3315bb
Ali Raza <ali@boldwp.com>
2025-11-30 14:47:39 +0000

    Server fix

--- app/server/src/app.ts
+++ app/server/src/app.ts
@@ -19,10 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 if (env.nodeEnv !== 'test') {
