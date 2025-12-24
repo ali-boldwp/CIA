5cc6b5a292761ead1160b176da4d33946f4081b2
Zubair <zubair@devregion.com>
2025-12-24 17:20:10 +0000

    done

--- app/client/src/pages/admin/Pages/Categories/Task/Components/index.js
+++ app/client/src/pages/admin/Pages/Categories/Task/Components/index.js
@@ -1,15 +1,43 @@
-import React, {useState} from 'react';
 import Header from "../../../../../CategoryView/Components/Header";
+import React, { useState } from "react";
+import styles from "./Style.module.css";
+import Popup from "./Popup";
+
+
+function FoamFields() {
+
+
+    const [openAddModal, setOpenAddModal] = useState(false);
 
 
 
-function FoamFields(props) {
+    return (
+        <div className={styles.main}>
+            <Header />
+            {/* Card 1 */}
+            <div className={styles.topCard}>
+                <h3 className={styles.cardTitle}>
+                    <span>Câmpuri de formular</span>
+                </h3>
 
-    return(
-        <div className="CategoryView">
-            <Header/>
-            <div>Hy</div>
 
+                <span
+                    className={styles.addCategoryText}
+                    onClick={() => setOpenAddModal(true)}
+                >
+  + Adaugă câmpuri de formular
+</span>
+
+            </div>
+
+            {/* Popup */}
+            {openAddModal && (
+                <Popup
+                    isOpen={openAddModal}
+                    onClose={() => setOpenAddModal(false)}
+
+                />
+            )}
         </div>
     );
 }
