b8e1ed6f6c8a1de87ff464213bc6c38bcc2435e7
Ubaid <ukhaliqansari@gmail.com>
2025-12-24 20:43:55 +0500

    Css Fixer

---
+++ app/client/src/pages/admin/Pages/Categories/Chapter/Page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../../../../../CategoryView/Components/Header";
+
+function Index(props) {
+    return (
+        <>
+        <Header />
+        <div>Hy</div>
+            </>
+    );
+}
+
+export default Index;
