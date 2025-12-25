03802dc16e0f038f2e55850a667007eee78abe24
Shoaib Khaliq <shoaib@devregion.com>
2025-12-25 20:24:08 +0000

    done .. .. ..

--- app/client/src/pages/admin/Pages/Projects/Tasks/View/Components/TaskView.js
+++ app/client/src/pages/admin/Pages/Projects/Tasks/View/Components/TaskView.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
-import Details from './Details'
+import React, { useState } from 'react';
+import { useParams } from "react-router-dom";
+import Details from './Details';
 import TaskFieldForm from "./TaskFieldForm";
 
 const TaskView = () => {
-    const {id:projectId,taskId}=useParams();
+    const { id: projectId, taskId } = useParams();
 
+
+    const [formValues, setFormValues] = useState({});
 
     return (
         <div>
-        <Details projectId={projectId} taskId={taskId} />
-        <TaskFieldForm taskId={taskId} />
+            <Details
+                projectId={projectId}
+                taskId={taskId}
+                formValues={formValues}
+            />
+
+            <TaskFieldForm
+                taskId={taskId}
+                formValues={formValues}
+                setFormValues={setFormValues}
+            />
         </div>
     );
 };
